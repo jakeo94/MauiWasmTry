a0b895d5f3528c4c1d3539b36462ea59d66f1e91
Jake Olson <jake._olson@yahoo.com>
2022-10-01 20:14:59 -0400

    Blank MAUI App

---
+++ MauiSignalRApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiSignalRApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MauiSignalRApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiSignalRApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
